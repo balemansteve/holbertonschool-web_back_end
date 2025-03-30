b95fc4456e6162c93e51de9dea68bd6288884d89
balemansteve <balemansteve@gmail.com>
2025-03-30 17:04:50 -0300

    Implement report object with department count

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,14 @@
+export default function createReportObject(employeesList) {
+  const allEmployees = {};
+
+  for (const [departmentName, employees] of Object.entries(employeesList)) {
+    allEmployees[departmentName] = [...employees];
+  }
+
+  const getNumberOfDepartments = () => Object.keys(allEmployees).length;
+
+  return {
+    allEmployees,
+    getNumberOfDepartments,
+  };
+}
