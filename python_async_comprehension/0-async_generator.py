39e7ab8112e728dc50ee9aa18b05686af1632bcd
balemansteve <balemansteve@gmail.com>
2025-04-30 15:42:14 -0300

    Add function return and fix random method

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,12 +6,13 @@ with asyncio
 
 import asyncio
 import random
+from typing import AsyncGenerator
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     This function implement async generator yielding
     random numbers with delay
     """
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        yield random.uniform(0, 10)
