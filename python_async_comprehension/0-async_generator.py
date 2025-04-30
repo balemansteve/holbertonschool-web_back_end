db81f67232fae4ff9b72b9aa0ce5ea0015353d7d
balemansteve <balemansteve@gmail.com>
2025-04-30 15:55:33 -0300

    Add whitespace

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,6 +8,7 @@ import asyncio
 import random
 from typing import Generator
 
+
 async def async_generator() -> Generator[float, None, None]:
     """
     This function implement async generator yielding
